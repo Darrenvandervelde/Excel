d7211bfaf21d03e7551abbc8c0bfcaec34e2868f
Darren van der Velde <darrenvandervelde@gmail.com>
2026-01-31 14:08:59 +0200

    Creatation index.php
    1. Testing Media Device for correct Media files
    2. Testing For VPN if on you will not be able to run this CRM.
    3. Testing IP Address if Banned the It Will Give you 404 Page. 
    4. If passed all three Testing then you will be redirect to the correct page based on the role or title you have.

---
+++ index.php
@@ -0,0 +1,8 @@
+<!-- INDEX FILE -->
+
+<!-- Testing Device Media files ---> 
+
+<!-- Testing For VPN | If VPN TRUE Divert to Take OFF VPN. --> 
+
+<!-- Testing for IP Address if Banned for using this CRM. -->
+
